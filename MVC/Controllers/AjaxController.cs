0677965bdbddf12b3ade0c3798aeb933ad176fe0
xu354cl4 <leehao891118@gmail.com>
2025-11-26 16:39:16 +0800

    加入專案檔案。

---
+++ MVC/Controllers/AjaxController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public AjaxController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        //GET: Ajax/Greet
+        [HttpGet]
+        public string Greet(string Name)
+        {
+            return $"Hello, {Name}!";
+        }
+        //POST: Ajax/PostGreet
+        [HttpPost]
+        public string PostGreet(string Name)
+        {
+            return $"Hello, {Name}!";
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
